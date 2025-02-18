034362a23711356a0c49bd89a8a7c93a7fd2e640
lucius <scamiros@gmail.com>
2025-02-18 10:11:50 +0100

    Controller e index page for Owners Closes #12

---
+++ src/main/java/org/lucius/petclinic/controllers/OwnersController.java
@@ -0,0 +1,13 @@
+package org.lucius.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnersController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
