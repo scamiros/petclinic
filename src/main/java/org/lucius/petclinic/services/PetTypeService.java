fb04f263b4ae266b866624049fbbde0f1c4fc6ee
lucius <scamiros@gmail.com>
2025-02-23 21:44:35 +0100

    Update Closes #29

---
+++ src/main/java/org/lucius/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package org.lucius.petclinic.services;
+
+import org.lucius.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ src/main/java/org/lucius/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,43 @@
+package org.lucius.petclinic.services.map;
+
+import org.lucius.petclinic.model.PetType;
+import org.lucius.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return super.existsById(id);
+    }
+
+    @Override
+    public long count() {
+        return super.count();
+    }
+}
