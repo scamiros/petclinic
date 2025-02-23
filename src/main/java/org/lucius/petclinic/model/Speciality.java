185097a98c3c878b5341188e7d24bddd3b162379
lucius <scamiros@gmail.com>
2025-02-23 20:02:08 +0100

    Update Closes #27

---
+++ src/main/java/org/lucius/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package org.lucius.petclinic.model;
+
+public class Speciality {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
